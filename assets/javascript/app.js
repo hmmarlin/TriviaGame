82cb5227f7e6b0fbc668d030795565c9db597d5a
Hannah Marlin <hmmarlin@gmail.com>
2016-08-02 21:51:54 -0700

    add progress on app.js and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,101 +1,184 @@
-// add array for images here
-	// one array to capture clue images
-	// one array to capture answer images
-
-// create object of questions? 
-var questions =[ 
-	question1 = {
-		'question': 'how many peas in a pod?',
-		'a': '3 peas in a pod',
-		'b': '1 peas in a pod',
-		'c': '2 peas in a pod',
-		'answer': 'a'
-	},
-	question2 = {
-		'question': 'how many dogs in a pool?',
-		'a': '3 dogs in a pool',
-		'b': '1 dogs in a pool',
-		'c': '2 dogs in a pool',
-		'answer': 'c'
-	},	
-]
-
-console.log(questions[1].question);
-
-// global variables
-var wins=0;
-var losses=0;
-var selection;
-
-// stopwatch
-var counter;
-
-var stopwatch = {
-    time: 10,
-    reset: function () {
-        stopwatch.time = 30;
-        $('stopwatch').html('00:00')
-    },
+ $(document).ready(function() {
+    $("#score").hide();
 
 
-    start: function(){
-        counter = setInterval(stopwatch.count, 1000)
-        
-    },
-    stop: function(){
-        clearInterval(counter);
+     // create object of questions:
+     var questions = [{
+         'question': 'What is Jay Z\'s real name?',
+         'a': 'Michael Knowles',
+         'b': 'Jeremy Johnson',
+         'c': 'Shawn Carter',
+         'answer': 'c',
+         'img': 'http://media.giphy.com/media/xT8qBmLfZXBDPBP29O/giphy.gif'
+     }, {
+         'question': 'What is Eminem\'s real name?',
+         'a': 'Marshall Mathers',
+         'b': 'Jimmy Smith, Jr.',
+         'c': 'B. Rabbit',
+         'answer': 'a',
+         'img': 'http://media.giphy.com/media/12jcwKEZNh4GSQ/giphy.gif'
+
+     }, {
+         'question': 'What is Drake\'s real name?',
+         'a': 'Aubrey Graham',
+         'b': 'Dennis Jones',
+         'c': 'Wayne Jenkins',
+         'answer': 'a',
+         'img': 'http://media.giphy.com/media/3o85xKWHrNvXqAvWMM/giphy.gif'
+     }, {
+         'question': 'What is Nicki Minaj\'s real name?',
+         'a': 'Nicolette Moore',
+         'b': 'Onika Maraj',
+         'c': 'Nicole Majors',
+         'answer': 'b',
+         'img': 'http://media.giphy.com/media/yliFuJ98lafBe/giphy.gif'
+     }, {
+         'question': 'What is Dr. Dre\'s real name?',
+         'a': 'Andre Young',
+         'b': 'Warren Greene',
+         'c': 'Marcel Roberts',
+         'answer': 'a',
+         'img': 'http://media.giphy.com/media/oL3v6WK8vVUac/giphy.gif'
+     }, {
+         'question': 'What is Da Brat\'s real name?',
+         'a': 'Ciara Wilson',
+         'b': 'Warren Greene',
+         'c': 'Shawntae Harris',
+         'answer': 'c',
+         'img': 'http://media.giphy.com/media/BRHJlhQStZ01q/giphy.gif'
+     }]
 
 
-    },
+     // global variables
+     var wins = 0;
+     var losses = 0;
+     var selection;
+     var i = 0;
+     var imgVal;
 
-    count: function(){
-        stopwatch.time--;
-        var converted=stopwatch.timeConverter(stopwatch.time);
-        $('#stopwatch').html(converted);
-        if (stopwatch.time == 0) {
-        	alert("time's up!");
-        	stopwatch.stop();
-        }
-    },
+     // stopwatch
+     var counter;
 
-    timeConverter: function(t){
-        var seconds = t;
-        if (seconds < 10){
-            seconds = "0" + seconds;
-        }
-        return '00' + ":" + seconds;
-    }
-};
+     var stopwatch = {
+         time: 10,
+         reset: function() {
+             stopwatch.stop();
+             stopwatch.time = 10;
 
-
-// create function to start game on click of button
-$('#start-button').on('click', displayQuestion);
-
-function displayQuestion (){
-  $('#button').hide();
-  stopwatch.start();
-  
+             //$('stopwatch').html('00:00')
+             displayQuestion();
+         },
 
 
-  for (i=0; i<questions.length; i++){
-  $('#question').html('<p>' + questions[i].question + '</p>');
-  $('#answer-a').html('<button id = a>' + questions[i].a + '</button>');
-  $('#answer-b').html('<button id = b>' + questions[i].b + '</button>');  
-  $('#answer-c').html('<button id = c>' + questions[i].c + '</button>');
+         start: function() {
+             counter = setInterval(stopwatch.count, 1000)
 
-		if (questions[i].answer === selection){
-					  		alert('great job!')
-					  		wins++;
-					  		stopwatch.reset();
-					  	}
+         },
+         stop: function() {
+             clearInterval(counter);
 
-					  	else if (questions[i].answer !== selection){
-					  		alert('loser!')
-					  		losses--;
-					  		stopwatch.reset();
-					  	}
-	
 
-			}
-	};
+         },
 
+         count: function() {
+             stopwatch.time--;
+             var converted = stopwatch.timeConverter(stopwatch.time);
+             $('#stopwatch').html(converted);
+             if (stopwatch.time == 0) {
+                 // alert("time's up!");
+                 stopwatch.reset();
+
+             }
+         },
+
+         timeConverter: function(t) {
+             var seconds = t;
+             if (seconds < 10) {
+                 seconds = "0" + seconds;
+             }
+             return '00' + ":" + seconds;
+         }
+     };
+
+
+
+     // create function to start game on click of button
+     $('#start-button').on('click', displayQuestion);
+
+     // create function to restart the game @ end on click of restart button
+     function restartGame() {
+         $('h3').show();
+         $('#loss').html('');
+         $('#wins').html('');
+         $('#answers').html('');
+         $('#start-button').show();
+         $("#game-over").html('')
+         console.log(questions.length);
+         wins = 0;
+         losses = 0;
+         i = (i - questions.length);
+
+
+     };
+
+     // at the end of the game, instead of displaying a new question, display results. 
+     function dispalyResults() {
+         $('#question').html('');
+         $('#a').html('');
+         $('#b').html('');
+         $('#c').html('');
+         stopwatch.stop();
+         $("#game-over").html('Game Over!');
+         $('#answers').append('<p>' + '<img src=' + 'http://media2.popsugar-assets.com/files/thumbor/vwXZq0YrIvHoQz9Tq0I2gfN8MUY/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/09/03/787/n/1922398/0dc7867952fbc808_beyonce-gif/i/Beyonce-Dancing-GIFs.gif' + '>' + '</p>')
+         $('#answers').append('<button class=restart> Play Again</button>');
+         $('.restart').on('click', restartGame);
+
+
+
+     }
+
+
+     // function to display all of my questions, in a for loop to loop thru the array of questions & options:
+
+     function displayQuestion() {
+         $('#start-button').hide();
+         $('h3').hide();
+         stopwatch.start();
+         $("#score").show();
+
+         // to end the question asking:
+         if (i >= questions.length) {
+             dispalyResults();
+         } else {
+
+             $('#question').html('<p>' + questions[i].question + '</p>');
+             $('#a').html('<button class="btn" id = a> a ' + '</button>' + questions[i].a);
+             $('#b').html('<button class="btn" id = b> b ' + '</button>' + questions[i].b)
+             $('#c').html('<button class="btn" id = c> c ' + '</button>' + questions[i].c);
+             $(".btn").click(function() {
+                 selection = this.id;
+
+                 if (questions[i - 1].answer === selection) {
+                     wins++;
+                     stopwatch.reset();
+                     $('#wins').html('Wins: ' + wins);
+
+
+
+
+                 } else if (questions[i - 1].answer !== selection) {
+                     losses++;
+                     $('#loss').html('Losses: ' + losses);
+                     stopwatch.reset();
+
+                 }
+
+             })
+
+             i++;
+         }
+     }
+     //display wins & losses 
+
+
+ });
